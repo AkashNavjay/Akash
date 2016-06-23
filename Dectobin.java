a3f9cc3ce97b95d408996ce44fbef346f9149537
AkashNavjay <n.aki1195@gmail.com>
2016-06-24 00:20:32 +0530

    Update Dectobin.java

--- Dectobin.java
+++ Dectobin.java
@@ -1,18 +1,20 @@
 	import java.io.*;
 	import java.util.Scanner;
-  public class Dectobin 
-  {
-   public static void main(String args[])
-	 {
-	  Scanner in=new Scanner(System.in);
-	  int num;
-	  System.out.println("Enter the Decimal:");
-	  num= in.nextInt();
-	  dtob(num);
-	 }
-	 public static void dtob(int num)
-	 {
-	  int dec[]=new int[10];
+        public class Dectobin
+       {
+        public static void main(String args[])
+        {
+	 Scanner in=new Scanner(System.in);
+	 int num;
+	 System.out.println("Enter the Decimal:");
+	 num= in.nextInt();
+	 dtob(num);
+        }
+        public static void dtob(int num)
+	{
+	 if(num>0)
+         {
+          int dec[]=new int[10];
 	  int rem,index;
 	  index = 0;
 	  while(num>=1)
@@ -23,8 +25,11 @@
 	  }
 	  System.out.println("Binary value:");
 	  for(int i=index-1;i>=0;i--)
-	  {
-	   System.out.print(dec[i]);
-	  }
-   } 
-  }
+	        {
+	            System.out.print(dec[i]);
+	        }
+	 }
+         else
+          System.out.print("Cannot be converted:");
+	}
+       }
