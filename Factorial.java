133c0e87c03441fe63b80fa9d83213165be53b41
AkashNavjay <n.aki1195@gmail.com>
2016-06-24 06:37:53 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.Scanner;
+public class Factorial {
+   
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        int n,fac;
+        System.out.println("Enter the no:");
+        n=in.nextInt();
+        if(n>0)
+        {
+            fac=fact(n);                                    /*Invoking fact method*/
+            System.out.println("Factorial:"+fac);
+        }
+        else
+            System.out.println("Factorial Cannot Be Found");
+        
+    }
+    static int fact(int n)
+    {
+        if(n>=1)
+            return n*fact(n-1);                                /*Recursively invoking fact*/
+        else
+            return 1;
+    }
+    
+}
