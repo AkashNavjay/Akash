eaa140d2af68b612af64ad0d4881e66721119da5
AkashNavjay <n.aki1195@gmail.com>
2016-06-24 06:41:50 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -6,7 +6,7 @@ public class Factorial {
     {
         Scanner in=new Scanner(System.in);
         int n,fac;
-        System.out.println("Enter the no:");
+        System.out.println("Enter a Positive no:");
         n=in.nextInt();
         if(n>0)
         {
@@ -14,7 +14,7 @@ public class Factorial {
             System.out.println("Factorial:"+fac);
         }
         else
-            System.out.println("Factorial Cannot Be Found");
+            System.out.println("Factorial:0");
         
     }
     static int fact(int n)
